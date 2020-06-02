657a933a1ee7ac940b7efc8642a66bbf28b81fb7
jadecodespy <jadekanimodo@gmail.com>
2020-06-02 09:31:22 +0100

    New Python files

---
+++ datatypes.py
@@ -0,0 +1,21 @@
+
+#strings
+word1 = "Good"
+word2 = "Day"
+word3 = "Jadesola"
+
+sentence= word1 + "" + word2 +"" + word3
+
+print(sentence)
+
+#floats 
+number1 = input("Enter whole number: ")
+number2 = input("Enter decimal number: ")
+
+integer_number = int(number1)
+float_number = float(number2)
+round_number = int(round(float_number))
+
+print(number1)
+print(number2)
+print(round_number)
